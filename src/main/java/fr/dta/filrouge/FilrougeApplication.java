007c518dc109f57e6bafb6a0a5b21aed5bce15b2
jerome ho-chui <jho-chui@DIGINAMIC.LOCAL>
2018-01-19 09:43:29 +0100

    first commit

---
+++ src/main/java/fr/dta/filrouge/FilrougeApplication.java
@@ -0,0 +1,12 @@
+package fr.dta.filrouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilrougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilrougeApplication.class, args);
+	}
+}
