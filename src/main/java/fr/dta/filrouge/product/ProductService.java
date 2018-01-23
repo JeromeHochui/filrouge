6a2f005638e456e02cddd4102e180e39fba8a055
Jeremy THUAL <jthual@DIGINAMIC.LOCAL>
2018-01-23 11:25:27 +0100

    réglage probleme git

--- src/main/java/fr/dta/filrouge/product/ProductService.java
+++ src/main/java/fr/dta/filrouge/product/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Service
@@ -19,4 +20,9 @@ public class ProductService {
 	public List<Product> getByCriteria(String name, Long id, Type type) {
 		return repository.findByCriteria(name, id, type);
 	}
+
+	public void store(MultipartFile file) {
+		// TODO Auto-generated method stub
+	}
+	
 }
