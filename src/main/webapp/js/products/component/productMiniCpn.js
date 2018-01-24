0eb86f021108f2d601c877d7b73c6be311fca64d
Allemand Guillaume <allemandg.contact@gmail.com>
2018-01-24 12:00:13 +0100

    yolooooooo

--- src/main/webapp/js/products/component/productMiniCpn.js
+++ src/main/webapp/js/products/component/productMiniCpn.js
@@ -5,9 +5,13 @@ angular.module('products').component('productMini', {
 	templateUrl: './partial/tpl-miniature-produit.html',
 	controller: ['ordersBasketSrv', function( ordersBasketSrv){
 		
-		
+		this.addToBasket = function(product, quantity){
+			ordersBasketSrv.addToBasket(product, quantity);
+			console.log('addToBasket', product);
+		}
 	}],
 	bindings: {
-		product: "="
+		product: "=",
+		quantity: "="
 	}	
 });
