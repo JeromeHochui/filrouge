c91ea0ba0400f6d95ef952a469096959f22566c9
Pierre GUYARD <pguyard@DIGINAMIC.LOCAL>
2018-01-30 16:33:26 +0100

    Modification OdersCtrl.js, ajout du total en prix et en quantite

--- src/main/webapp/js/orders/controller/OrdersCtrl.js
+++ src/main/webapp/js/orders/controller/OrdersCtrl.js
@@ -1,29 +1,23 @@
 angular.module('orders').controller('OrdersCtrl',
 		['$scope', '$rootScope', '$routeParams', '$location', '$cookies', 'titre', 'ordersBasketSrv', 'ordersProductsSrv', function($scope, $rootScope, $routeParams, $location, $cookies, titre, ordersBasketSrv, ordersProductsSrv){
 		
-		$scope.order = [];
+		$scope.order = undefined;
 		$rootScope.titre = titre.libelle;
 		var id = $routeParams.id;
 		
 		ordersProductsSrv.getOrder(id).then(function(data){
-			$scope.order = data;	
-		});
-		
-		function calculPrixTotal(){
+			$scope.order = data;
+
 			$scope.totalPrice = 0;
 			angular.forEach($scope.order.quantities, function(value, key){
 				$scope.totalPrice += (value.quantity * value.products.price);
-			})
-		}
-		calculPrixTotal();
-		
-		function calculQuantityTotal(){
+			});
+			
 			$scope.totalQuantity = 0;
 			angular.forEach($scope.order.quantities, function(value, key){
 				$scope.totalQuantity += value.quantity;
-			})
-		}
-		calculQuantityTotal();
-		
-		
+			});
+
+		});
+
 }]);
